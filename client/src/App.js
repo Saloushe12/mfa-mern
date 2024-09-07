e70214957077757eb03aed18b5198bc6799cbbb3
Jonathan Zhao <132334671+Saloushe12@users.noreply.github.com>
2024-09-07 17:35:31 -0400

    added CSS to login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Login from './components/Login';
+import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <div style={centerStyle}>
-        <h1>
-          MFA using MERN Stack
+        <h1 className='title'>
+          Sample Login with Multi-Factor Authentification
         </h1>
       </div>
       <Login />
